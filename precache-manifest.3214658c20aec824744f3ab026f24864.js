bd1c390b385f437bb7a343c23cf4ac5252386462
Max Xu <mxu@kayak.com>
2020-03-07 00:28:27 -0500

    init

---
+++ precache-manifest.3214658c20aec824744f3ab026f24864.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2afa17a40bb34886f3f339939f05bc20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cc6371f28ace7b42b95",
+    "url": "/static/css/0.65b39531.chunk.css"
+  },
+  {
+    "revision": "3436df713c7067830844",
+    "url": "/static/css/4.9e5da2c1.chunk.css"
+  },
+  {
+    "revision": "63fcd25640e03b6d2df6",
+    "url": "/static/css/7.96443f8b.chunk.css"
+  },
+  {
+    "revision": "1aa5c8be11d1de9d6b1f",
+    "url": "/static/css/main.a6319325.chunk.css"
+  },
+  {
+    "revision": "0cc6371f28ace7b42b95",
+    "url": "/static/js/0.7c1c31b6.chunk.js"
+  },
+  {
+    "revision": "a0d6181b54ab2ae79815",
+    "url": "/static/js/10.0b31b74e.chunk.js"
+  },
+  {
+    "revision": "38a63efe6c274d0f1053",
+    "url": "/static/js/3.c58c3a2b.chunk.js"
+  },
+  {
+    "revision": "d7c817b0ebe435ce063ee8f485b2c580",
+    "url": "/static/js/3.c58c3a2b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3436df713c7067830844",
+    "url": "/static/js/4.687c2b2f.chunk.js"
+  },
+  {
+    "revision": "46d65c31ffd77d4b17b5",
+    "url": "/static/js/5.6b3b69fd.chunk.js"
+  },
+  {
+    "revision": "bc4ac7f32a2d52017c1f1a16be561e99",
+    "url": "/static/js/5.6b3b69fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b765527025bb00c68d6",
+    "url": "/static/js/6.be2e5821.chunk.js"
+  },
+  {
+    "revision": "63fcd25640e03b6d2df6",
+    "url": "/static/js/7.22a9eca9.chunk.js"
+  },
+  {
+    "revision": "7624290b22fc9b5ec0fa",
+    "url": "/static/js/8.c141dbdb.chunk.js"
+  },
+  {
+    "revision": "455792d5e002126b5ff3",
+    "url": "/static/js/9.e2fcd403.chunk.js"
+  },
+  {
+    "revision": "1aa5c8be11d1de9d6b1f",
+    "url": "/static/js/main.6da49207.chunk.js"
+  },
+  {
+    "revision": "40cfadad3bf179e4f79e",
+    "url": "/static/js/runtime-main.025f6c0b.js"
+  },
+  {
+    "revision": "d36999d0015618049ea958fc96aaed1b",
+    "url": "/static/media/icon-404.d36999d0.png"
+  },
+  {
+    "revision": "ed48530654e49ba30feeb63f9536c344",
+    "url": "/static/media/location.ed485306.svg"
+  },
+  {
+    "revision": "f97d912d3040a48fe950204d12f2918e",
+    "url": "/static/media/review-empty.f97d912d.svg"
+  },
+  {
+    "revision": "c1ba2d58aaafbfefb98d9dcdae081020",
+    "url": "/static/media/review-half.c1ba2d58.svg"
+  },
+  {
+    "revision": "80c669fd87a32d6b1181f48c021ae3c1",
+    "url": "/static/media/review-star.80c669fd.svg"
+  }
+]);
